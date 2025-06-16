946cbc9f60406f4dc113eb55c0df9fcb8db32c69
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-06-17 02:55:49 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gurur on 17.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
