701bf8839149cb55bdaeabcd589564930c5276f8
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-06-17 04:51:17 +0300

    Enhance UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,7 +9,14 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp: Date = Date.now
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+
+    @State private var wakeUp: Date = defaultWakeTime
     @State private var sleepAmount: Double = 8.0
     @State private var coffeeAmount: Int = 1
     
@@ -19,22 +26,24 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
+            Form {
                 Text("When do you want to wake up?")
                     .font(.headline)
                 
                 DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
                 
                 Text("Daily coffee intake")
                     .font(.headline)
                 
-                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
             }
             .navigationTitle("BetterRest")
             .toolbar {
@@ -45,7 +54,6 @@ struct ContentView: View {
             } message: {
                 Text(alertMessage)
             }
-            .padding()
         }
     }
     
